3aa2fb9d609b0d643cb8b3ad07a96a2de21a46ed
samiullah997 <samiullahk997@gmail.com>
2022-12-20 12:24:31 +0500

    Generate model for expense and Implement validation

---
+++ app/models/expense.rb
@@ -0,0 +1,8 @@
+class Expense < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { scope: :author, message: 'You already have an transaction with this name' }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  belongs_to :author, class_name: 'User'
+  has_many :group_expenses, dependent: :destroy
+  has_many :groups, through: :group_expenses, dependent: :destroy
+end
