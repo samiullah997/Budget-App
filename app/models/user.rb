871d14f1ea620e01cb5c2db43470f09208ae41d7
samiullah997 <samiullahk997@gmail.com>
2022-12-20 12:23:39 +0500

    Generate model for user and Implement validation

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 15 }
+
+
+    has_many :groups, foreign_key: :author_id, dependent: :destroy
+    has_many :expenses, foreign_key: :author_id, dependent: :destroy
+end
