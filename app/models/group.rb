5310d6433d33b6d6abf619b84650b4933bbf13c8
samiullah997 <samiullahk997@gmail.com>
2022-12-22 01:01:42 +0500

    Update group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,13 @@
 class Group < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 },
                    uniqueness: { scope: :author, message: 'You have already Category with this name' }
-  validates :icon, presence: true, length: { maximum: 50 }
+  validates :icon, presence: true, length: { maximum: 250 }
 
   belongs_to :author, class_name: 'User'
   has_many :group_expenses, dependent: :destroy
   has_many :expenses, through: :group_expenses, dependent: :destroy
+
+  def total_transactions
+    expenses.sum(:amount)
+  end
 end
