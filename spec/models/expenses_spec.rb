f934efa8cfd636023ed329b191ffbd241a496b40
samiullah997 <samiullahk997@gmail.com>
2022-12-22 01:17:15 +0500

    Update user data in spec to test the data.

--- spec/models/expenses_spec.rb
+++ spec/models/expenses_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe Expense, type: :model do
   before :each do
     @user = User.create(name: 'Samiullah', email: 'samiullahk997@gmail.com', password: 'samiullah')
-    @transaction = Expense.new(author: @user, name: 'Andor', amount: 12.99)
+    @transaction = Expense.new(author: @user, name: 'Bat', amount: 100)
   end
 
-  context 'Testing Validations' do
-    it 'is valid with valid attributes' do
+  context 'This Testing Validations' do
+    it 'is valid with all valid attributes' do
       expect(@transaction).to be_valid
     end
 
-    it 'is not valid without a name' do
+    it 'is not valid without a transaction name' do
       @transaction.name = nil
       expect(@transaction).to_not be_valid
     end
 
-    it 'is not valid without an icon' do
+    it 'is not valid without an transaction icon' do
       @transaction.amount = nil
       expect(@transaction).to_not be_valid
     end
@@ -25,19 +25,19 @@ RSpec.describe Expense, type: :model do
       expect(@transaction.amount).to be_kind_of Numeric
     end
 
-    it 'is not valid without user id' do
+    it 'is not valid without transaction user id' do
       @transaction.author_id = nil
       expect(@transaction).to_not be_valid
     end
   end
 
-  context 'Testing Associations' do
-    it 'belongs_to User' do
+  context 'This Testing Associations' do
+    it 'is belongs_to User' do
       assoc = Expense.reflect_on_association(:author)
       expect(assoc.macro).to eq :belongs_to
     end
 
-    it 'has_many group_expenses' do
+    it 'is has_many group_expenses' do
       assoc = Expense.reflect_on_association(:group_expenses)
       expect(assoc.macro).to eq :has_many
     end
--- spec/models/group_expense_spec.rb
+++ spec/models/group_expense_spec.rb
@@ -8,29 +8,29 @@ RSpec.describe GroupExpense, type: :model do
     @category_transaction = GroupExpense.new(group: @category, expense: @transaction)
   end
 
-  context 'Testing Validations' do
-    it 'is valid with valid attributes' do
+  context 'This Testing Validations' do
+    it 'is valid with all attributes' do
       expect(@category_transaction).to be_valid
     end
 
-    it 'is valid without a group_id' do
+    it 'is valid without a category group_id' do
       @category_transaction.group_id = nil
       expect(@category_transaction).to_not be_valid
     end
 
-    it 'is valid without an expense_id' do
+    it 'is valid without an category expense_id' do
       @category_transaction.expense_id = nil
       expect(@category_transaction).to_not be_valid
     end
   end
 
-  context 'Testing Associations' do
-    it 'belongs_to recipe' do
+  context 'This Testing Associations' do
+    it 'is belongs_to recipe' do
       assoc = GroupExpense.reflect_on_association(:group)
       expect(assoc.macro).to eq :belongs_to
     end
 
-    it 'belongs_to food' do
+    it 'is belongs_to food' do
       assoc = GroupExpense.reflect_on_association(:expense)
       expect(assoc.macro).to eq :belongs_to
     end
